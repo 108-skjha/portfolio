ad5d8be4746fe998fecaf341eac9ba5191001186
Shrawan kumar jha <shrawanjha2004@gmail.com>
2025-07-13 19:22:22 +0530

    first commit

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I have been consistently developing my experties in <i><b className="purple">coding, networking, AI & Data Science</b></i>
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple">C, C++, python. </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b> and
+                also in areas related to{" "}
+                <b className="purple">
+                  AI and data science.
+                </b>
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also apply my passion for developing
+               <b className="purple"> Websites</b> 
+              <i>
+                <b className="purple">
+                  {" "}
+                  
+                </b>
+              </i>
+              &nbsp;
+              <i>
+                <b className="purple"></b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/108-skjha"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://x.com/shrawanjha2004?t=lj9vQ0O34t5-QhOTuBtArw&s=09"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/shrawan-kumar-jha-92b3342b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/kashyap_shrawan108?igsh=MXNvOWl2YXFvZmg0bA=="
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import editor from "../../Assets/Projects/tic.jpeg";
+import chatify from "../../Assets/Projects/vapi1.jpeg";
+import bitsOfCode from "../../Assets/Projects/poetry.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Codenixo"
+              description="My project is a voice-based assistant built with Vapi that helps users discover website ideas based on their needs. It suggests suitable domains and interacts with users to understand their budget, making the website planning process easy and conversational."
+              linkedinLink="https://www.linkedin.com/posts/shrawan-kumar-jha-92b3342b3_agenticai-voiceai-vapi-activity-7324039866877403136-Oz12?utm_source=share&utm_medium=member_android&rcm=ACoAAEtVZzMBndHvWuYRfpBZoWwQPJXAP3FXVOI"
+
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Poetry hub"
+              description="Poetry Hub is a creative website I designed and developed to celebrate the art of poetry. Built using HTML, CSS, and JavaScript, it offers a clean and responsive interface for users to read and enjoy timeless and original poems. The site features a sidebar clock, intuitive navigation, and a visually rich layout to enhance the reading experience."
+              ghLink="https://108-skjha.github.io/poetry_web/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Editor.io"
+              description="Tic Tac Toe Game is a fun and interactive web-based game I developed using HTML, CSS, and JavaScript. It features a simple yet engaging user interface, responsive design, and real-time game logic that allows two players to compete in classic X and O style. The project demonstrates my understanding of DOM manipulation, event handling, and logic implementation in JavaScript."
+              ghLink="https://github.com/108-skjha/tic-tac-toe"            
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
